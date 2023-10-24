750e294231aca2b23999f8c0d1a2b23069b91de9
Peter K. Tarnue <petertarnue@gmail.com>
2023-10-24 10:45:31 +0000

    new update
    new update

---
+++ envtesting/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/mac/Documents/GitHub/django-testing-tutorial/envtesting/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
